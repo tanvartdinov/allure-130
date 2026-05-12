477d75c14785c116c21d6795cc0378179c192695
tanvartdinov <tanvartdinov@users.noreply.github.com>
2026-05-12 15:55:19 +0000

    deploy: 70c2cebd540b07780890e53bb073ba301307ba9b

--- utils/helpers.py
+++
@@ -1,7 +0,0 @@
-import allure
-
-
-@allure.step('Проверка статуса ответа')
-def assert_response_statuscode(response, expected_statuscode):
-    assert response.status_code == expected_statuscode, \
-        f'Ожидался статус {expected_statuscode}, но получен {response.status_code}'
